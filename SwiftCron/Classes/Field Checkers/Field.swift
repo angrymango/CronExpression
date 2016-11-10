23ceb4862cff91db5c614a910f1c5d7536a72e0d
Keegan Rush <keeganrush@gmail.com>
2016-11-10 06:33:32 +0200

    Remove use of ranges (-)

--- SwiftCron/Classes/Field Checkers/Field.swift
+++ SwiftCron/Classes/Field Checkers/Field.swift
@@ -1,58 +1,11 @@
 import Foundation
 
-private func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
-  switch (lhs, rhs) {
-  case let (l?, r?):
-    return l < r
-  case (nil, _?):
-    return true
-  default:
-    return false
-  }
-}
-
-private func >= <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
-  switch (lhs, rhs) {
-  case let (l?, r?):
-    return l >= r
-  default:
-    return !(lhs < rhs)
-  }
-}
-
-private func <= <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
-  switch (lhs, rhs) {
-  case let (l?, r?):
-    return l <= r
-  default:
-    return !(rhs < lhs)
-  }
-}
-
-
 class Field
 {
 	var fields: NSMutableArray?
 
 	func isSatisfied(_ dateValue: String, value: String) -> Bool
 	{
-		if isRange(value)
-		{
-			return isInRange(dateValue, withValue: value)
-		}
-
 		return value == CronRepresentation.DefaultValue || dateValue == value
-	}
-
-	func isRange(_ value: String) -> Bool
-	{
-		return value.range(of: CronRepresentation.RangeIdentifier) != nil
-	}
-
-	func isInRange(_ dateValue: String, withValue value: String) -> Bool
-	{
-		let parts = value.components(separatedBy: CronRepresentation.RangeIdentifier)
-
-		return Int(dateValue) >= Int(parts[0]) && Int(dateValue) <= Int(parts[1])
 	}
 }
