aaee9a2ba82f38b557d08eb91dddeeb75f7ac416
Keegan Rush <keeganrush@gmail.com>
2017-03-11 04:49:31 +0200

    Create Project file and target for Carthage

---
+++ SwiftCron/SwiftCron.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCron.h
+//  SwiftCron
+//
+//  Created by Keegan Rush on 2017/03/11.
+//  Copyright © 2017 TheCodedSelf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCron.
+FOUNDATION_EXPORT double SwiftCronVersionNumber;
+
+//! Project version string for SwiftCron.
+FOUNDATION_EXPORT const unsigned char SwiftCronVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCron/PublicHeader.h>
+
+
